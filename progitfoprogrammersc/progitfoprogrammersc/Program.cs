96e3fb3d9965d526b92c48e38e32e793df92dda8
holdingline <1905298@uad.ac.uk>
2021-09-24 15:11:08 +0100

    first commit - from command line

---
+++ progitfoprogrammersc/progitfoprogrammersc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace progitfoprogrammersc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
