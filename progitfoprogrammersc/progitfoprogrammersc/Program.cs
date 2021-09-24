1feb6b9610b5afa873e69710eaa79471178075b0
holdingline <1905298@uad.ac.uk>
2021-09-24 15:24:05 +0100

    info line

--- progitfoprogrammersc/progitfoprogrammersc/Program.cs
+++ progitfoprogrammersc/progitfoprogrammersc/Program.cs
@@ -7,6 +7,7 @@ namespace progitfoprogrammersc
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Added from VS");
         }
     }
 }
